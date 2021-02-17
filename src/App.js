08481dc1b6f7618615cc0e6e84d66d8d28d51b5a
jjuriz <jjuriz@gmail.com>
2021-02-17 18:21:06 -0500

    [style, Header] updated header nav content and styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-// import { Link } from "react-router-dom";
+
 import React, { useState, useEffect } from "react";
 import TextLoop from "react-text-loop";
+
 import Header from "./Header";
 import Footer from "./Footer";
 import "./main.scss";
--- src/Header.js
+++ src/Header.js
@@ -1,33 +1,51 @@
-import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles' // imports option to make custom styles
-import MenuIcon from '@material-ui/icons/Menu'
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles"; // imports option to make custom styles
+import { Link } from "react-router-dom";
 
+import About from "./About";
 // Function to create custom Material UI styles
-const useStyles = makeStyles({
-    helloThereStyles: {
-        fontStyle: 'oblique'
-    }
-});
+const useStyles = makeStyles((theme) => ({
+  menuButton: {
+    marginRight: theme.spacing(2),
+    textDecoration: "none",
+    color: "black",
+    transition: ".2s",
+
+    "&:hover": {
+      backgroundColor: "black",
+      color: "white",
+      textDecoration: "none",
+      padding: "16px 10px",
+    },
+  },
+}));
 
 function Header() {
-    const classes = useStyles(); // allows use of custom styles on page
-    return (
-        <header className="App-header">
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography
-                        className={classes.helloThereStyles} // GOING TO NEED TO CHANGE THIS
-                        variant="h6">
-                        About
-                    </Typography>
-                    <Button color="inherit">home</Button>
-                </Toolbar>
-            </AppBar>
-        </header>
-    )
+  const classes = useStyles(); // allows use of custom styles on page
+  return (
+    <header className="App-header">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography className={classes.menuButton} variant="h6">
+            <Link className={classes.menuButton} to="/profile">Profile</Link>
+          </Typography>
+          <Typography className={classes.menuButton} variant="h6">
+            <Link className={classes.menuButton} to="/books">Books</Link>
+          </Typography>
+          <Typography className={classes.menuButton} variant="h6">
+            <Link className={classes.menuButton} to="/poems-essays-more">
+              Poems, Essays, and More
+            </Link>
+          </Typography>
+          <Typography className={classes.menuButton} variant="h6">
+            <Link className={classes.menuButton} to="/about">
+              About
+            </Link>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </header>
+  );
 }
 
 export default Header;
